7ce453a5514451061b2d99379e02e9d330c8b307
Saintmalik <msalawu5@gmail.com>
2021-05-23 18:36:04 +0100

    still testing with DO

--- public/default/js/fire.js
+++ public/default/js/fire.js
@@ -1,66 +1,65 @@
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyD9CtmOnvTgzesdH3YBzquIuHrARtfeU_M",
-  authDomain: "mssn-49bdd.firebaseapp.com",
-  databaseURL: "https://mssn-49bdd.firebaseio.com",
-  projectId: "mssn-49bdd",
-  storageBucket: "mssn-49bdd.appspot.com",
-  messagingSenderId: "476170176254",
-  appId: "1:476170176254:web:5e346b7fc10a62c07e87af",
-  measurementId: "G-H7BEX31T8D"
+    apiKey: "AIzaSyD9CtmOnvTgzesdH3YBzquIuHrARtfeU_M",
+    authDomain: "mssn-49bdd.firebaseapp.com",
+    databaseURL: "https://mssn-49bdd.firebaseio.com",
+    projectId: "mssn-49bdd",
+    storageBucket: "mssn-49bdd.appspot.com",
+    messagingSenderId: "476170176254",
+    appId: "1:476170176254:web:5e346b7fc10a62c07e87af",
+    measurementId: "G-H7BEX31T8D",
 };
 
 firebase.initializeApp(config);
 
-  
-  // Reference messages collection
-  var messagesRef = firebase.database().ref('Alumnis');
-  
-  // Listen for form submit
-  document.getElementById('registerForm').addEventListener('submit', submitForm);
-  
-  // Submit form
-  function submitForm(e){
+// Reference messages collection
+var messagesRef = firebase.database().ref("Alumnis");
+
+// Listen for form submit
+document.getElementById("registerForm").addEventListener("submit", submitForm);
+
+// Submit form
+function submitForm(e) {
     e.preventDefault();
-  
+
     // Get values
-    const name = getInputVal('name');
-    const department = getInputVal('department');
-    const faculty = getInputVal('faculty');
-    const year = getInputVal('year');
-    const phone = getInputVal('phone');
-    const email = getInputVal('email');
-    const whatsno = getInputVal('whatsno');
-  
+    const name = getInputVal("name");
+    const department = getInputVal("department");
+    const faculty = getInputVal("faculty");
+    const year = getInputVal("year");
+    const phone = getInputVal("phone");
+    const email = getInputVal("email");
+    const whatsno = getInputVal("whatsno");
+
     // Save message
     saveMessage(name, department, faculty, year, phone, email, whatsno);
 
-     // Show alert
-  document.querySelector('.alert').style.display = 'block';
+    // Show alert
+    document.querySelector(".alert").style.display = "block";
 
-  // Hide alert after 3 seconds
-  setTimeout(function(){
-    document.querySelector('.alert').style.display = 'none';
-  },20000);
+    // Hide alert after 3 seconds
+    setTimeout(function() {
+        document.querySelector(".alert").style.display = "none";
+    }, 20000);
 
     // Clear form
-  document.getElementById('registerForm').reset();
-  }
-  // Function to get get form values
-  function getInputVal(id){
+    document.getElementById("registerForm").reset();
+}
+// Function to get get form values
+function getInputVal(id) {
     return document.getElementById(id).value;
-  }
-  
-  // Save message to firebase
-  function saveMessage(name, department, faculty, year, phone, email, whatsno){
+}
+
+// Save message to firebase
+function saveMessage(name, department, faculty, year, phone, email, whatsno) {
     var newMessageRef = messagesRef.push();
     newMessageRef.set({
-      name: name,
-      department:department,
-      faculty:faculty,
-      year:year,
-      phone:phone,
-      email:email,
-      whatsno:whatsno
+        name: name,
+        department: department,
+        faculty: faculty,
+        year: year,
+        phone: phone,
+        email: email,
+        whatsno: whatsno,
     });
-  }
+}
--- public/sw.js
+++ public/sw.js
@@ -1,54 +1,58 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('mssn-eksu').then((cache) => cache.addAll([
-      '/',
-      'app.js',
-      'default/css/main.css',
-      'default/css/media.css',
-      'default/css/head&foot.css',
-      'default/js/main.js',
-      'default/images/mssn-eksu.png',
-      'default/css/bootstrap/bootstrap.min.css',
-      'default/js/bootstrap/bootstrap.min.js',
-      'default/js/wow.min.js',
-      'default/css/404.css',
-      'default/css/about.css',
-      'default/css/animate.css',
-      'default/css/donate.css',
-      'default/css/eksuma.css',
-      'default/css/excos.css',
-      'default/css/reg.css',
-      'default/css/submit.css',
-      'default/css/events.css',
-      'default/images/abdul.jpeg',
-      'default/images/adigun.jpg',
-      'default/images/aisha.jpg',
-      'default/images/banner.jpg',
-      'default/images/bar.jpg',
-      'default/images/idayat.jpg',
-      'default/images/jar.jpg',
-      'default/images/kareem.jpg',
-      'default/images/logo.png',
-      'default/images/logo2.PNG',
-      'default/images/nhicoms.jpg',
-      'default/images/rasak.jpg',
-      'default/images/wahab.jpg',
-      'default/images/zainab.jpg',
-    ])),
-  );
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+        caches
+        .open("mssn-eksu")
+        .then((cache) =>
+            cache.addAll([
+                "/",
+                "app.js",
+                "default/css/main.css",
+                "default/css/media.css",
+                "default/css/head&foot.css",
+                "default/js/main.js",
+                "default/images/mssn-eksu.png",
+                "default/css/bootstrap/bootstrap.min.css",
+                "default/js/bootstrap/bootstrap.min.js",
+                "default/js/jquery-3.6.0.min.js",
+                "default/js/wow.min.js",
+                "default/css/404.css",
+                "default/css/about.css",
+                "default/css/animate.css",
+                "default/css/donate.css",
+                "default/css/eksuma.css",
+                "default/css/excos.css",
+                "default/css/reg.css",
+                "default/css/submit.css",
+                "default/css/events.css",
+                "default/images/abdul.jpeg",
+                "default/images/adigun.jpg",
+                "default/images/aisha.jpg",
+                "default/images/banner.jpg",
+                "favicon.ico",
+                "default/images/bar.jpg",
+                "default/images/idayat.jpg",
+                "default/images/jar.jpg",
+                "default/images/kareem.jpg",
+                "default/images/logo.png",
+                "default/images/logo2.PNG",
+                "default/images/nhicoms.jpg",
+                "default/images/rasak.jpg",
+                "default/images/wahab.jpg",
+                "default/images/zainab.jpg",
+            ])
+        )
+    );
 });
 
-self.addEventListener('fetch', (e) => {
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
-  );
+self.addEventListener("fetch", (e) => {
+    console.log(e.request.url);
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request))
+    );
 });
 
-
-
-// 
-// 
+//
+//
 // 'default/js/fire.js',
-// 
-// 
+//
+//
